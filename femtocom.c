7fca9b47ef26d49c97c4038be21db5e25dfa5fd5
sid314 <sidaa314@gmail.com>
2026-07-05 22:13:28 +0530

    changed keybind, changed cleanup mechanism to run on nuttx

--- femtocom.c
+++ femtocom.c
@@ -2,20 +2,19 @@
 #include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/poll.h>
 #include <termios.h>
 #include <unistd.h>
 struct termios keyb_backup;
 void restore_terminal(void) {
   tcsetattr(STDIN_FILENO, TCSANOW, &keyb_backup);
-  printf("\nTerminal restored. Goodbye!\n");
+  printf("\r\nTerminal restored. Goodbye!\r\n");
 }
 int main(int argc, char *argv[]) {
   char *portname = NULL;
   char *baudrate = NULL;
-  char c;
-  while ((c = getopt(argc, argv, "p:b:")) != -1) {
-    switch (c) {
+  int opt;
+  while ((opt = getopt(argc, argv, "p:b:")) != -1) {
+    switch (opt) {
     case 'p':
       portname = optarg;
       break;
@@ -28,28 +27,31 @@ int main(int argc, char *argv[]) {
     printf("Usage: %s -p <port> -b <baud>\n", argv[0]);
     return EXIT_FAILURE;
   }
-  printf("Baud: %s\n", baudrate);
-  printf("Rort: %s\n", portname);
+  printf("   Port: %s\n", portname);
+  printf("   Baud: %s\n", baudrate);
+
   int fd = open(portname, O_RDWR | O_NOCTTY);
   if (fd < 0) {
     perror("Failed to open serial port");
     return EXIT_FAILURE;
   }
-  printf("Port %s opened at %s baud with file descriptor %d\n", portname,
-         baudrate, fd);
   struct termios keyb;
   struct termios port;
-  tcgetattr(STDIN_FILENO, &keyb);
-  tcgetattr(STDIN_FILENO, &keyb_backup);
+  if (tcgetattr(STDIN_FILENO, &keyb) < 0) {
+    perror("Warning: Failed to get stdin attributes");
+  }
+  keyb_backup = keyb;
   atexit(restore_terminal);
-  tcgetattr(fd, &port);
+  if (tcgetattr(fd, &port) < 0) {
+    perror("Warning: Failed to get serial port attributes");
+  }
   speed_t speed;
   switch (atoi(baudrate)) {
   case 115200:
     speed = B115200;
     break;
   default:
-    printf("Unsupported speed");
+    printf("Unsupported speed\n");
     return EXIT_FAILURE;
   }
   cfsetispeed(&port, speed);
@@ -58,27 +60,35 @@ int main(int argc, char *argv[]) {
   cfmakeraw(&keyb);
   tcsetattr(STDIN_FILENO, TCSANOW, &keyb);
   tcsetattr(fd, TCSANOW, &port);
+  fsync(STDOUT_FILENO);
   struct pollfd fds[2];
   fds[0].fd = STDIN_FILENO;
   fds[0].events = POLLIN;
   fds[1].fd = fd;
   fds[1].events = POLLIN;
   while (1) {
-    poll(fds, 2, -1);
+    int ret = poll(fds, 2, -1);
+    if (ret < 0) {
+      break;
+    }
     if (fds[0].revents & POLLIN) {
-      char c;
-      read(STDIN_FILENO, &c, 1);
-      if (c == 0x11) {
-        break;
+      char tx_char;
+      if (read(STDIN_FILENO, &tx_char, 1) > 0) {
+        if (tx_char == 0x18) {
+          break;
+        }
+        write(fd, &tx_char, 1);
       }
-      write(fd, &c, 1);
     }
     if (fds[1].revents & POLLIN) {
-      char c;
-      read(fd, &c, 1);
-      write(STDOUT_FILENO, &c, 1);
+      char rx_char;
+      if (read(fd, &rx_char, 1) > 0) {
+        write(STDOUT_FILENO, &rx_char, 1);
+        fsync(STDOUT_FILENO);
+      }
     }
   }
   close(fd);
+  restore_terminal();
   return EXIT_SUCCESS;
 }
